fe2076dd399b35958fb5dc98e99ffe2122a22061
Lin Han <davidparkerhl@gmail.com>
2019-10-13 18:15:57 +0800

    ADD

--- gtk/button.c
+++ gtk/button.c
@@ -1,41 +1,147 @@
+#include <stdio.h>
 #include <gtk/gtk.h>
 
-static void print_hello(GtkWidget *widget,gpointer data)
+/*
+ * callback event function for the gtk delete event. invoked
+ * when the application is closed using the window manager.
+ */
+int
+delete_event(GtkWidget *widget, GdkEvent *event, gpointer data)
 {
-  g_print("hello world\n");
+    /* If you return FALSE in the "delete_event" signal handler,
+     * GTK will emit the "destroy" signal. Returning TRUE means
+     * you don't want the window to be destroyed.
+     * This is useful for popping up 'are you sure you want to quit?'
+     * type dialogs.
+     */
+
+    return(FALSE);
 }
 
-static void activate(GtkApplication *app,gpointer user_data)
+
+/* exit the application. */
+void
+exit_cb(GtkWidget *widget, gpointer data)
 {
-  GtkWidget *window;
-  GtkWidget *button;
-  GtkWidget *button_box;
-
-  window = gtk_application_window_new(app);
-  gtk_window_set_title(GTK_WINDOW(window),"my window");
-  gtk_window_set_default_size(GTK_WINDOW(window),200,200);
-
-  button_box=gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
-  gtk_container_add(GTK_CONTAINER(window),button_box);
-
-  button=gtk_button_new_with_label("click button");
-  g_signal_connect(button,"clicked",G_CALLBACK(print_hello),NULL);
-  g_signal_connect_swapped(button,"clicked",G_CALLBACK(gtk_widget_destroy),window);
-  gtk_container_add(GTK_CONTAINER(button_box),button);
-
-  gtk_widget_show_all(window);
-
+    gtk_main_quit();
 }
 
-int main(int argc,char ** argv)
+/* "print text" button "clicked" callback function. */
+void
+on_button1_clicked(GtkButton* button, gpointer data)
 {
-  GtkApplication *app;
-  int status;
+    /* cast the data back to a GtkEntry*  */
+    GtkEntry* entry = (GtkEntry*)data;
 
-  app=gtk_application_new("org.button.example",G_APPLICATION_FLAGS_NONE);
-  g_signal_connect(app,"activate",G_CALLBACK(activate),NULL);
-  status=g_application_run(G_APPLICATION(app),argc,argv);
-  g_object_unref(app);
+    printf("on_button_clicked - entry = '%s'\n", gtk_entry_get_text(entry));
+    fflush(stdout);
+}
 
-  return status;
+/* "toggle editability" button "clicked" callback function. */
+void
+on_button2_clicked(GtkButton* button, gpointer data)
+{
+    static gboolean is_editable = TRUE;
+    /* cast the data back to a GtkEntry*  */
+    GtkEntry* entry = (GtkEntry*)data;
+
+    if (is_editable) {
+	gtk_entry_set_editable(entry, FALSE);
+        is_editable = FALSE;
+    }
+    else {
+	gtk_entry_set_editable(entry, TRUE);
+        is_editable = TRUE;
+    }
+}
+
+int
+main(int argc, char* argv[])
+{
+    /* this variable will store a pointer to the window object. */
+    GtkWidget* main_window;
+    /* this will store a horizontal box. */
+    GtkWidget* hbox;
+    /* these will store push buttons. */
+    GtkWidget* button1;
+    GtkWidget* button2;
+    /* this will store a text entry. */
+    GtkWidget* entry;
+
+
+    /* This is called in all GTK applications. Arguments */
+    /* are parsed from the command line and are returned */
+    /* to the application.                               */
+    gtk_init(&argc, &argv);
+
+    /* create a new top-level window. */
+    main_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+
+    /* define some of the window's attributes. */
+    gtk_window_set_title(GTK_WINDOW (main_window),
+                         "A Top Window For The Rest Of Us...");
+    gtk_container_set_border_width(GTK_CONTAINER (main_window), 5);
+
+    /* make the window visible. */
+    gtk_widget_show(main_window);
+
+    /* When the window is given the "delete_event" signal (this is given
+     * by the window manager, usually by the "close" option, or on the
+     * titlebar), we ask it to call the delete_event() function
+     * as defined above. The data passed to the callback
+     * function is NULL and is ignored in the callback function.
+     */
+    gtk_signal_connect(GTK_OBJECT (main_window), "delete_event",
+                       GTK_SIGNAL_FUNC (delete_event), NULL);
+
+    /* Here we connect the "destroy" event to a signal handler.
+     * This event occurs when we call gtk_widget_destroy() on the window,
+     * or if we return FALSE in the "delete_event" callback.
+     */
+    gtk_signal_connect(GTK_OBJECT (main_window), "destroy",
+                       GTK_SIGNAL_FUNC (exit_cb), NULL);
+
+    /* create a new horizontal box, and add to top-level window. */
+    hbox = gtk_hbox_new(TRUE, 20);
+    gtk_container_add(GTK_CONTAINER(main_window), hbox);
+    gtk_widget_show(hbox);
+
+    /* create a text entry. */
+    entry = gtk_entry_new_with_max_length(20);
+
+    /* insert the following text into the text entry, as its initial value. */
+    gtk_entry_set_text(GTK_ENTRY(entry), "hello there");
+
+    /* make the text entry visible. */
+    gtk_widget_show(entry);
+
+    /* add the text entry to the hbox. */
+    gtk_box_pack_start(GTK_BOX(hbox), entry, FALSE, TRUE, 1);
+
+    /* create a button, place in hbox, attach callback.   */
+    /* note: button is created second, cause it needs the */
+    /* entry's pointer as a parameter to its callback.    */
+    button1 = gtk_button_new_with_label("Print Text");
+    gtk_widget_show(button1);
+    gtk_box_pack_start(GTK_BOX(hbox), button1, FALSE, TRUE, 1);
+    gtk_signal_connect(GTK_OBJECT(button1), "clicked",
+                       GTK_SIGNAL_FUNC(on_button1_clicked),
+                       (gpointer)entry);
+
+    /* create a 2nd button, place in hbox, attach callback. */
+    button2 = gtk_button_new_with_label("Toggle Editablibity");
+    gtk_widget_show(button2);
+    gtk_box_pack_start(GTK_BOX(hbox), button2, FALSE, TRUE, 1);
+    gtk_signal_connect(GTK_OBJECT(button2), "clicked",
+                       GTK_SIGNAL_FUNC(on_button2_clicked),
+                       (gpointer)entry);
+
+
+    /* All GTK applications must have a gtk_main(). Control
+     * ends here and waits for an event to occur (like a
+     * key press or mouse event).
+     */
+    gtk_main();
+
+    exit(0);
 }
---
+++ hash/GTK/jtRecord.c
@@ -0,0 +1,88 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <unistd.h>
+#include <string.h>
+#include <gtk/gtk.h>
+#include <gtk/gtkx.h>
+#include <math.h>
+#include <ctype.h>
+
+
+
+// Make them global
+
+GtkWidget	*window;
+GtkWidget	*entry_key;
+GtkWidget	*entry_value;
+GtkWidget	*but_add;
+GtkWidget	*but_del;
+GtkWidget	*but_search;
+GtkWidget	*hash_data_pre;
+GtkWidget	*hash_data_now;
+
+GtkBuilder	*builder;
+GError *error = NULL;
+
+
+
+int main(int argc, char *argv[])
+{
+
+
+
+	gtk_init(&argc, &argv); // init Gtk
+
+//---------------------------------------------------------------------
+// establish contact with xml code used to adjust widget settings
+//---------------------------------------------------------------------
+
+  builder = gtk_builder_new_from_file ("layout.glade");
+
+	window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
+  g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+  gtk_builder_connect_signals(builder, NULL);
+
+  entry_key = GTK_WIDGET(gtk_builder_get_object(builder,"entry_key"));
+  entry_value = GTK_WIDGET(gtk_builder_get_object(builder,"entry_value"));
+  but_add = GTK_WIDGET(gtk_builder_get_object(builder,"but_add"));
+  but_del = GTK_WIDGET(gtk_builder_get_object(builder,"but_del"));
+  but_search = GTK_WIDGET(gtk_builder_get_object(builder,"but_search"));
+  hash_data_pre = GTK_WIDGET(gtk_builder_get_object(builder,"hash_data_pre"));
+  hash_data_now = GTK_WIDGET(gtk_builder_get_object(builder,"hash_data_now"));
+
+	gtk_widget_show(window);
+
+	gtk_main();
+
+	return EXIT_SUCCESS;
+}
+
+void on_but_add_clicked (GtkButton *b)
+{
+  gchar *buf;
+  buf=g_strdup(gtk_entry_get_text(GTK_ENTRY(entry_key)));
+
+
+
+	gtk_label_set_text (GTK_LABEL(hash_data_pre), buf);
+}
+
+void  on_but_del_clicked (GtkButton *b)
+{
+	gtk_label_set_text (GTK_LABEL(hash_data_pre), (const gchar* ) "Hello World");
+}
+
+void	on_but_search_clicked (GtkButton *b)
+{
+	gtk_label_set_text (GTK_LABEL(hash_data_now), (const gchar* ) "Hello World");
+}
+
+
+
+
+/*
+gcc   jtRecord.c  -lm `pkg-config --cflags --libs gtk+-3.0` -export-dynamic -rdynamic
+gcc -Wno-format -o hash jtRecord.c -Wno-deprecated-declarations -Wno-format-security -lm `pkg-config --cflags --libs gtk+-3.0` -export-dynamic
+ */
